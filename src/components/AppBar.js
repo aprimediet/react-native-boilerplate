4ab3f9b2318af26a89d5bfa775c41c41e6721604
Aditya Prima <aprimediet@gmail.com>
2018-07-14 03:00:10 +0700

    Added conditional render on right toolbar in AppBar

---
+++ src/components/AppBar.js
@@ -0,0 +1,59 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { Toolbar, ToolbarBackAction, ToolbarAction, ToolbarContent } from 'react-native-paper'
+
+
+export default class AppBar extends PureComponent {
+  static propTypes = {
+    index: PropTypes.number.isRequired,
+    scene: PropTypes.object.isRequired,
+    navigation: PropTypes.object.isRequired,
+    navigationOptions: PropTypes.oneOfType([PropTypes.object, PropTypes.func]).isRequired,
+  }
+
+  navigateBack = () => {
+    // Actions when back button is pressed
+    const { navigation } = this.props
+
+    navigation.goBack(null)
+  }
+
+  openDrawer = () => {
+    // Actions when menu button is pressed
+    const { navigation } = this.props
+
+    navigation.openDrawer()
+  }
+
+  renderToolbarRight = () => {
+    // If headerLeft is provided in navigationOptions, return headerLeft
+    // else, return back button when index is not 0
+    const {
+      index,
+      scene: {
+        descriptor: {
+          options: { headerLeft },
+        },
+      },
+    } = this.props
+
+    if (headerLeft) {
+      return headerLeft
+    } else {
+      if (index > 0) {
+        return <ToolbarBackAction onPress={this.navigateBack} />
+      }
+
+      return <ToolbarAction icon="menu" onPress={this.openDrawer} />
+    }
+  }
+
+  render() {
+    return (
+      <Toolbar>
+        {this.renderToolbarRight()}
+        <ToolbarContent title="Hello Paper" />
+      </Toolbar>
+    )
+  }
+}
