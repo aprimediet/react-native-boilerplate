836431f507233ccb3d289e2d345ffbfc2a0af538
Aditya Prima <aprimediet@gmail.com>
2018-07-14 23:14:17 +0700

    Fixing wrong import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux'
 import { Provider as PaperProvider } from 'react-native-paper'
 import store from '@/stores'
 
-import DrawerNavigator from '@/navigator'
+import { DrawerNavigator } from '@/navigator'
 
 export default () => (
   <Provider store={store}>
