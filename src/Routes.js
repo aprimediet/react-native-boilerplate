1170bd884b58fd157ad6aa7a85c03efbe587fd52
Aditya Prima <aprimediet@gmail.com>
2018-07-14 22:42:21 +0700

    removed unused files for base

--- src/Routes.js
+++
@@ -1,9 +0,0 @@
-import Home from '@/screens/Home'
-import About from '@/screens/About'
-import Search from '@/screens/Search'
-
-export default {
-  Home: { screen: Home },
-  About: { screen: About },
-  Search: { screen: Search },
-}
--- src/navigator/index.js
+++
@@ -1,2 +0,0 @@
-export { default as RootNavigator } from './RootNavigator'
-export { default as DrawerNavigator } from './DrawerNavigator'
