8a506e07425329703a18e911a17ba3537c127d03
Aditya Prima <aprimediet@gmail.com>
2018-07-19 02:15:05 +0700

    Updated HomeScreen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,28 +1,68 @@
 /**
  * Home Screen
  */
-import React, { PureComponent } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { StyleSheet, View } from 'react-native'
+import { POSTS_FETCH } from '@/constants/ActionTypes'
+import BaseComponent from '@/components/BaseComponent'
 import { Title } from 'react-native-paper'
 
-@connect((state) => ({ http: state.get('http').toJS() }))
-class Home extends PureComponent {
+@connect(
+  (state) => ({
+    http: state.get('http').toJS(),
+    posts: state.get('posts').toJS(),
+  }),
+  (dispatch) => ({
+    fetchPosts({ page }) {
+      dispatch({ type: POSTS_FETCH, page })
+    },
+  })
+)
+class Home extends BaseComponent {
   static propTypes = {
-    http: PropTypes.any,
+    http: PropTypes.object,
+    posts: PropTypes.object,
+    fetchPosts: PropTypes.func,
+  }
+
+  state = {
+    currentPage: 1,
   }
 
   componentDidMount() {
-    const { http } = this.props
-
-    console.log(http) //eslint-disable-line no-console
+    const { currentPage } = this.state
+    this.fetch(currentPage)
+    // console.log(http) //eslint-disable-line no-console
+    // console.log(posts) //eslint-disable-line no-console 
   }
 
+  fetch = (page) => {
+    const { fetchPosts } = this.props
+
+    fetchPosts({
+      page,
+    })
+  }
+
+  // renderLoading = () => <Title style={styles.instructions}>Loading...</Title>
+
   render() {
+    const {
+      http: { isFetching, error },
+      posts: { total },
+    } = this.props
+
+    if (isFetching) return this.renderLoading()
+    if (error) return this.renderError(error.response.data.message)
+
     return (
       <View style={styles.container}>
-        <Title style={styles.instructions}>Home Screen</Title>
+        <View>
+          <Title style={styles.instructions}>Home Screen</Title>
+          <Title style={styles.instructions}>Total Row: {total}</Title>
+        </View>
       </View>
     )
   }
